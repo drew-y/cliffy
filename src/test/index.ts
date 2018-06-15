f97bbcc58d506731f236dc81e1e46e4c7fe939cb
Drew Youngwerth <drew.youngwerth@versabuilt.com>
2018-06-15 14:28:27 -0600

    Use custom stream mock package

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,14 +1,15 @@
 import { CLI } from "../index";
-import { stdio } from "stdio-mock";
 import assert = require("assert");
+import { MockReadableStream, MockWriteableStream } from "mock-streams";
 
-const { stdin, stdout } = stdio();
-const send = (msg: string) => stdin.write(`${msg}\n`);
+const input = new MockReadableStream();
+const output = new MockWriteableStream();
+const send = (msg: string) => input.push(`${msg}\n`);
 
 describe("CLI", function() {
     let cli: CLI;
     beforeEach(function() {
-        cli = new CLI({ output: stdout as any, input: stdin! as any });
+        cli = new CLI({ output, input });
         cli.show();
     });
 
@@ -54,7 +55,7 @@ describe("CLI", function() {
                 action(params) {
                     const correct = params.p1 === "hello" && params.p2 === "world" &&
                         params.p3 === 4 && params.p4 === false && params.p5 === true;
-                    if (correct) { res(); } else { rej(); }
+                    if (correct) { res(); } else { rej(new Error("Bad params")); }
                 }
             });
         });
@@ -68,7 +69,7 @@ describe("CLI", function() {
                 options: ["hello", "world"],
                 action(params, opts) {
                     const correct = opts.hello && !opts.world;
-                    if (correct) { res(); } else { rej(); }
+                    if (correct) { res(); } else { rej(new Error("Bad opts")); }
                 }
             });
         });
