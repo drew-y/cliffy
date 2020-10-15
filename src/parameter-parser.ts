4de879eab81e3f43570e989def49f23e72aa5f94
Drew Youngwerth <drew@youngwerth.com>
2020-10-15 16:08:19 -0600

    Check to ensure user does not pass more parameters than defined on a command.

--- src/parameter-parser.ts
+++ src/parameter-parser.ts
@@ -22,6 +22,21 @@ function requiredParameterCount(parameters: (Parameter | string)[]) {
     return count;
 }
 
+function optionalParameterCount(parameters: (Parameter | string)[]) {
+    let count = 0;
+    for (const param of parameters) {
+        if (typeof param === "string") continue;
+        if (param.optional || param.rest) count += 1;
+    }
+    return count;
+}
+
+function hasRestParam(parameters: (Parameter | string)[]) {
+    const lastParam = parameters[parameters.length - 1];
+    if (!lastParam || typeof lastParam === "string") return false;
+    return !!lastParam.rest;
+}
+
 /**
  * Get the parameters object from the remainingPieces of the command string.
  *
@@ -33,7 +48,14 @@ function requiredParameterCount(parameters: (Parameter | string)[]) {
 export function parseParameters(command: Command, commandPieces: string[]): any | false {
     if (!command.parameters && (commandPieces.length > 0)) return false;
     if (!command.parameters) return {};
-    if (commandPieces.length < requiredParameterCount(command.parameters)) {
+
+    // Ensure the user passed all required parameters.
+    const requiredCount = requiredParameterCount(command.parameters);
+    if (commandPieces.length < requiredCount) return false;
+
+    // Ensure the user did not provide extra parameters.
+    const totalParameters = requiredCount + optionalParameterCount(command.parameters);
+    if (commandPieces.length > totalParameters && !hasRestParam(command.parameters)) {
         return false;
     }
 
